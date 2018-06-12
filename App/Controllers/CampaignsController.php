369d73b43fc4cda07b39bedc5a965b3b803f2f22
Wancieho <wancieho@gmail.com>
2018-06-12 11:44:54 +0200

    cleanup MysqliHandler as empty tables where throwing foreach errors

--- App/Controllers/CampaignsController.php
+++ App/Controllers/CampaignsController.php
@@ -8,77 +8,77 @@ use App\Objects\Campaign;
 
 class CampaignsController {
 
-    /**
-     * @url POST /
-     * @return object
-     */
-    public function create() {
-        if (!isset($_POST['name']) || empty($_POST['name'])) {
-            throw new RestException(401, 'Field `name` must be defined to create a campaign');
-        }
+	/**
+	 * @url POST /
+	 * @return object
+	 */
+	public function create() {
+		if (!isset($_POST['name']) || empty($_POST['name'])) {
+			throw new RestException(401, 'Field `name` must be defined to create a campaign');
+		}
 
-        return CampaignsModel::save(new Campaign((object) [
-                            'name' => $_POST['name'],
-        ]));
-    }
+		return CampaignsModel::save(new Campaign((object) [
+							'name' => $_POST['name'],
+		]));
+	}
 
-    /**
-     * @url GET /
-     * @return array
-     */
-    public function readAll() {
-        $campaigns = CampaignsModel::get();
+	/**
+	 * @url GET /
+	 * @return array
+	 */
+	public function readAll() {
+		$campaigns = CampaignsModel::get();
 
-        foreach ($campaigns as &$val) {
-            unset($val['id']);
-        }
+		foreach ($campaigns as &$val) {
+			unset($val['id']);
+		}
 
-        return $campaigns;
-    }
+		return $campaigns;
+	}
 
-    /**
-     * @url GET /$id
-     * @return object
-     */
-    public function read($id = null) {
-        $campaign = CampaignsModel::get((object) ['id' => $id]);
+	/**
+	 * @url GET /$id
+	 * @return object
+	 */
+	public function read($id = null) {
+		$campaign = CampaignsModel::get($id);
 
-        unset($campaign->id);
+		unset($campaign->id);
 
-        return $campaign;
-    }
+		return $campaign;
+	}
 
-    /**
-     * @url PUT /$id
-     * @return object
-     */
-    public function update($id = null) {
-        $_PUT = [];
+	/**
+	 * @url PUT /$id
+	 * @return object
+	 */
+	public function update($id = null) {
+		$_PUT = [];
 
-        parse_str(file_get_contents('php://input'), $_PUT);
+		parse_str(file_get_contents('php://input'), $_PUT);
 
-        $campaign = CampaignsModel::update(new Campaign((object) [
-                            'id' => $id,
-                            'name' => $_PUT['name'],
-        ]));
+		$campaign = CampaignsModel::update(new Campaign((object) [
+							'id' => $id,
+							'name' => $_PUT['name'],
+		]));
 
-        return $campaign;
-    }
+		return $campaign;
+	}
 
-    /**
-     * @url DELETE /
-     * @return object
-     */
-    public function deleteAll() {
-        return CampaignsModel::delete();
-    }
+	/**
+	 * @url DELETE /
+	 * @return object
+	 */
+	public function deleteAll() {
+		return CampaignsModel::delete();
+	}
 
-    /**
-     * @url DELETE /$id
-     * @return object
-     */
-    public function delete($id = null) {
-        return CampaignsModel::delete($id);
-    }
+	/**
+	 * @url DELETE /$id
+	 * @return object
+	 */
+	public function delete($id = null) {
+		return CampaignsModel::delete($id);
+	}
 
 }
--- App/Utils/MysqliHandler.php
+++ App/Utils/MysqliHandler.php
@@ -10,261 +10,261 @@ use Jacwright\RestServer\RestException;
 
 class MysqliHandler {
 
-    static public $table = '';
+	static public $table = '';
 
-    /*
-     * Creates an entry in the specified table
-     * 
-     * @return object
-     */
+	/*
+	 * Creates an entry in the specified table
+	 * 
+	 * @return object
+	 */
 
-    static public function save($data = null) {
-        $vals = [];
-        $keys = [];
-        $inserts = [];
+	static public function save($data = null) {
+		$vals = [];
+		$keys = [];
+		$inserts = [];
 
-        foreach ($data as $key => $val) {
-            if (!is_null($val)) {
-                $keys[] = $key;
-                $vals[] = $val;
-                $inserts[] = '?';
-            }
-        }
+		foreach ($data as $key => $val) {
+			if (!is_null($val)) {
+				$keys[] = $key;
+				$vals[] = $val;
+				$inserts[] = '?';
+			}
+		}
 
-        $query = 'INSERT INTO ' . static::$table . ' (' . join(',', $keys) . ') VALUES (' . join(',', $inserts) . ')';
+		$query = 'INSERT INTO ' . static::$table . ' (' . join(',', $keys) . ') VALUES (' . join(',', $inserts) . ')';
 
-        $statement = DB::handler()->prepare($query);
+		$statement = DB::handler()->prepare($query);
 
-        if (!$statement) {
-            // #TODO: move into reusable method
-            throw new RestException(401, 'Error executing query `' . $query . '`');
-        }
+		if (!$statement) {
+			// #TODO: move into reusable method
+			throw new RestException(401, 'Error executing query `' . $query . '`');
+		}
 
-        // #TODO: put this into reusable method
-        $dataTypes = '';
+		// #TODO: put this into reusable method
+		$dataTypes = '';
 
-        foreach ($data as $key => $val) {
-            if (is_string($val)) {
-                $dataTypes .= 's';
-            } elseif (is_int($val)) {
-                $dataTypes .= 'i';
-            }
-        }
+		foreach ($data as $key => $val) {
+			if (is_string($val)) {
+				$dataTypes .= 's';
+			} elseif (is_int($val)) {
+				$dataTypes .= 'i';
+			}
+		}
 
-        $params = [];
+		$params = [];
 
-        // add data types to bind params array
-        $params[] = &$dataTypes;
+		// add data types to bind params array
+		$params[] = &$dataTypes;
 
-        // add values to bind params array
-        foreach ($vals as $key => $val) {
-            $params[] = &$vals[$key];
-        }
+		// add values to bind params array
+		foreach ($vals as $key => $val) {
+			$params[] = &$vals[$key];
+		}
 
-        // dynamically call bind_param method based on array of fields to save
-        call_user_func_array(array($statement, 'bind_param'), $params);
+		// dynamically call bind_param method based on array of fields to save
+		call_user_func_array(array($statement, 'bind_param'), $params);
 
-        $statement->execute();
+		$statement->execute();
 
-        self::queryError($statement);
+		self::queryError($statement);
 
-        // store inserted ID for return
-        $id = $statement->insert_id;
+		// store inserted ID for return
+		$id = $statement->insert_id;
 
-        $statement->close();
+		$statement->close();
 
-        return (object) ['id' => $id];
-    }
+		return (object) ['id' => $id];
+	}
 
-    /*
-     * Reads either a single table row or multiple rows depending on params
-     * 
-     * @return mixed
-     */
+	/*
+	 * Reads either a single table row or multiple rows depending on params
+	 * 
+	 * @return mixed
+	 */
 
-    static public function get($params = null) {
-        $data = [];
-        $where = isset($params->where) ? $params->where : [];
-        $order = isset($params->order) ? ' ' . $params->order : '';
-        $limit = isset($params->limit) ? ' ' . $params->limit : '';
+	static public function get($params = null) {
+		$data = [];
+		$where = [];
+		$order = isset($params->order) ? ' ' . $params->order : '';
+		$limit = isset($params->limit) ? ' ' . $params->limit : '';
 
-        if (isset($params->id)) {
-            // rebuild ID into object for dynamic where building
-            $where = ['id' => $params->id];
-        }
+		if (is_string($params) || is_integer($params)) {
+			$where = (object) ['id' => (integer) $params];
+		} elseif (isset($params->where) && is_array($params->where)) {
+			$where = $params->where;
+		} elseif (isset($params->id)) {
+			$where = (object) ['id' => (integer) $params->id];
+		}
 
-        $query = 'SELECT * FROM ' . static::$table . self::buildWhere($where) . $order . $limit;
+		$query = 'SELECT * FROM ' . static::$table . self::buildWhere($where) . $order . $limit;
 
-        $statement = DB::handler()->prepare($query);
+		$statement = DB::handler()->prepare($query);
 
-        if (!$statement) {
-            throw new RestException(401, 'Error executing query `' . $query . '`');
-        }
+		if (!$statement) {
+			throw new RestException(401, 'Error executing query `' . $query . '`');
+		}
 
-        if ($statement) {
-            $statement->execute();
+		if ($statement) {
+			$statement->execute();
 
-            self::queryError($statement);
+			self::queryError($statement);
 
-            $result = $statement->get_result();
+			$result = $statement->get_result();
 
-            // insert all DB results into array for return
-            if ($result->num_rows > 1) {
-                while ($row = $result->fetch_assoc()) {
-                    $data[] = $row;
-                }
-            } elseif ($result->num_rows === 1) {
-                $data = (object) $result->fetch_assoc();
-            } else {
-                $data = null;
-            }
+			if (is_string($params) || is_integer($params) || isset($params->id)) {
+				$data = (object) $result->fetch_assoc();
+			} else {
+				while ($row = $result->fetch_assoc()) {
+					$data[] = $row;
+				}
+			}
 
-            $statement->close();
+			$statement->close();
 
-            return $data;
-        }
+			return $data;
+		}
 
-        return null;
-    }
+		return null;
+	}
 
-    /*
-     * Update table row
-     * 
-     * @return object
-     */
+	/*
+	 * Update table row
+	 * 
+	 * @return object
+	 */
 
-    static public function update($data = null) {
-        $id = $data->id;
-        $vals = [];
-        $set = '';
+	static public function update($data = null) {
+		$id = $data->id;
+		$vals = [];
+		$set = '';
 
-        // remove ID form array to prevent any MySQL query issues
-        unset($data->id);
+		// remove ID form array to prevent any MySQL query issues
+		unset($data->id);
 
-        foreach ($data as $key => $val) {
-            if (!is_null($val)) {
-                if (!empty($set)) {
-                    $set .= ', ';
-                }
+		foreach ($data as $key => $val) {
+			if (!is_null($val)) {
+				if (!empty($set)) {
+					$set .= ', ';
+				}
 
-                $set .= $key . '=?';
-                $vals[] = $val;
-            }
-        }
+				$set .= $key . '=?';
+				$vals[] = $val;
+			}
+		}
 
-        $query = 'UPDATE ' . static::$table . ' SET ' . $set . ' WHERE id=' . $id;
+		$query = 'UPDATE ' . static::$table . ' SET ' . $set . ' WHERE id=' . $id;
 
-        $statement = DB::handler()->prepare($query);
+		$statement = DB::handler()->prepare($query);
 
-        if (!$statement) {
-            throw new RestException(401, 'Error executing query `' . $query . '`');
-        }
+		if (!$statement) {
+			throw new RestException(401, 'Error executing query `' . $query . '`');
+		}
 
-        $dataTypes = '';
+		$dataTypes = '';
 
-        foreach ($data as $key => $val) {
-            if (is_string($val)) {
-                $dataTypes .= 's';
-            } elseif (is_int($val)) {
-                $dataTypes .= 'i';
-            }
-        }
+		foreach ($data as $key => $val) {
+			if (is_string($val)) {
+				$dataTypes .= 's';
+			} elseif (is_int($val)) {
+				$dataTypes .= 'i';
+			}
+		}
 
-        $params = [];
+		$params = [];
 
-        // add data types to bind params array
-        $params[] = &$dataTypes;
+		// add data types to bind params array
+		$params[] = &$dataTypes;
 
-        // add values to bind params array
-        foreach ($vals as $key => $val) {
-            $params[] = &$vals[$key];
-        }
+		// add values to bind params array
+		foreach ($vals as $key => $val) {
+			$params[] = &$vals[$key];
+		}
 
-        // dynamically call bind_param method based on array of fields to save
-        call_user_func_array(array($statement, 'bind_param'), $params);
+		// dynamically call bind_param method based on array of fields to save
+		call_user_func_array(array($statement, 'bind_param'), $params);
 
-        $statement->execute();
+		$statement->execute();
 
-        $statement->close();
+		$statement->close();
 
-        return (object) ['id' => $id];
-    }
+		return (object) ['id' => $id];
+	}
 
-    /*
-     * Delete row from table
-     * 
-     * @return object
-     */
+	/*
+	 * Delete row from table
+	 * 
+	 * @return object
+	 */
 
-    static public function delete($params = null) {
-        $where = '';
-        $id = null;
+	static public function delete($params = null) {
+		$where = '';
+		$id = null;
 
-        if (is_int($params) || is_string($params)) {
-            $id = (integer) $params;
+		if (is_int($params) || is_string($params)) {
+			$id = (integer) $params;
 
-            $where = ' WHERE id=' . $id;
-        } elseif (isset($params->id)) {
-            $id = (integer) $params->id;
+			$where = ' WHERE id=' . $id;
+		} elseif (isset($params->id)) {
+			$id = (integer) $params->id;
 
-            $where = ' WHERE id=' . $id;
-        }
+			$where = ' WHERE id=' . $id;
+		}
 
-        $query = 'DELETE FROM ' . static::$table . $where;
+		$query = 'DELETE FROM ' . static::$table . $where;
 
-        $statement = DB::handler()->prepare($query);
+		$statement = DB::handler()->prepare($query);
 
-        $statement->execute();
+		$statement->execute();
 
-        self::queryError($statement);
+		self::queryError($statement);
 
-        $success = false;
+		$success = false;
 
-        if ($statement->affected_rows > 0) {
-            $success = true;
-        }
+		if ($statement->affected_rows > 0) {
+			$success = true;
+		}
 
-        $statement->close();
+		$statement->close();
 
-        if ($success) {
-            return true;
-        }
+		if ($success) {
+			return true;
+		}
 
-        return false;
-    }
+		return false;
+	}
 
-    /*
-     * Reusable query error method
-     * 
-     * @return string
-     */
+	/*
+	 * Reusable query error method
+	 * 
+	 * @return string
+	 */
 
-    static private function queryError($statement) {
-        if (!empty($statement->error)) {
-            throw new RestException(401, $statement->error);
-        }
-    }
+	static private function queryError($statement) {
+		if (!empty($statement->error)) {
+			throw new RestException(401, $statement->error);
+		}
+	}
 
-    /*
-     * Where claus Query string builder
-     * 
-     * @return string
-     */
+	/*
+	 * Where claus Query string builder
+	 * 
+	 * @return string
+	 */
 
-    static private function buildWhere($params = []) {
-        $where = '';
+	static private function buildWhere($params = []) {
+		$where = '';
 
-        foreach ($params as $key => $val) {
-            if (empty($where)) {
-                $where = ' WHERE ';
-            } elseif (!empty($where)) {
-                $where .= ' AND ';
-            }
+		foreach ($params as $key => $val) {
+			if (empty($where)) {
+				$where = ' WHERE ';
+			} elseif (!empty($where)) {
+				$where .= ' AND ';
+			}
 
-            $where .= $key . '=' . $val;
-        }
+			$where .= $key . '=' . $val;
+		}
 
-        return $where;
-    }
+		return $where;
+	}
 
 }
